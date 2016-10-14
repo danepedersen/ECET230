28bbeebc4558d9cd10e10dcbbd3f07b7e78b901b
David Pedersen <djtpedersen@gmail.com>
2016-10-14 14:06:27 -0700

    Populated window; first text box code added

---
+++ Lab2Calculator/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string inputstring;
+        double inputdouble;
+        double output;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            inputstring = Input.Text;
+            inputdouble = Convert.ToDouble(inputstring);
+        }
+    }
+}
